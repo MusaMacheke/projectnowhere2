c24b0406dc67fcf342e0cb4924b38d6426d9ecd8
MusaMacheke <thembamacheke.tm@gmail.com>
2025-01-03 17:12:19 +0200

    sixth commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+import "./vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
+
 module.exports = {
     resolve:{
         fallback:{
             process:require.resolve('process/browser')
         },
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
     },
 };
